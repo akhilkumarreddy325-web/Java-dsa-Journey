10f765058e052b8512f9b63b2cab1c7032f7e521
Akhil <akhilkumarreddy325@gmail.com>
2026-09-20 06:54:55 +0530

    reverse of numbers

--- DSA5.java
+++ DSA5.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
 public class DSA5 {
-
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter size:");
+        int n=sc.nextInt();
+        for(int i=n;i>0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+}
 }
--- dsa4.java
+++ dsa4.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class dsa4 {
      public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        System.out.print("enter size:");
         int n=sc.nextInt();
         for(int i=n;i>0;i--){
             for(int j=i;j>0;j--){
