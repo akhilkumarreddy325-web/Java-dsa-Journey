2cef05fe0c6e82da7035608788c789e879131cd3
Akhil <akhilkumarreddy325@gmail.com>
2026-09-21 06:35:03 +0530

    common elements in two arrays

--- arr52.java
+++ arr52.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
-public class arr51 {
+public class arr52 {
     public static void main(String[] args){
          Scanner sc=new Scanner(System.in);
         System.out.print("enter size:");
         int n=sc.nextInt();
         int arr[]=new int[n];
+        int arr1[]=new int[n];
         System.out.print("enter array elements ");
         for(int i=0;i<n;i++){
         arr[i]=sc.nextInt();}
-        int max=arr[0];
-
-    
+        System.out.print("Enter second array elements");
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                 int sum=0;
-                for(int k=i;k<=j;k++){
-              
-               sum+=arr[k];}
-               if(max<sum){
-                max=sum;
-               }
-            }
+            arr1[i]=sc.nextInt();
         }
-        System.out.print(max);
-}}
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+            if(arr[i]==arr1[j]){
+                System.out.print(arr[i]+" ");
+            }}
+        }
+}
+}
